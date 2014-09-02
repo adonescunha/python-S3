5f97de14fbaa5a25bc51ffb33cb0b0ea20d212b5
Adones Cunha <adonescunha@gmail.com>
2014-09-01 22:16:43 -0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# python-S3
+# https://github.com/adonescunha/python-S3
+
+
+from setuptools import setup
+
+
+setup(
+    name='python-S3',
+    version='0.1.0',
+    description='Amazon S3 Python binding.',
+    author='Adones Cunha',
+    author_email='adonescunha@gmail.com',
+    url='https://github.com/adonescunha/python-S3',
+    packages=[
+        'S3'
+    ],
+)
+
